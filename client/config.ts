0895a998ccf232585d9bfecc1d0b66449a98574e
George Fang <george@heycanopy.com>
2023-06-19 00:16:08 -0700

    init gfang/building-npm

--- client/config.ts
+++ client/config.ts
@@ -1,4 +1,10 @@
-export const config = {
-    development: "http://localhost:3001",
+type Config = {
+    [key: string]: string;
+    development: string;
+    production: string;
+};
+export const config: Config = {
+    // development: "http://localhost:3001",
+    development: "http://host.docker.internal:3001",
     production: "https://your-production-url.com",
 };
--- dist/client/config.d.ts
+++ dist/client/config.d.ts
@@ -1,4 +1,7 @@
-export declare const config: {
+type Config = {
+    [key: string]: string;
     development: string;
     production: string;
 };
+export declare const config: Config;
+export {};
